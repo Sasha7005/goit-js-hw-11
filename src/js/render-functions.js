49fe36e7e59a61379e27f48ebafbcedb295171bd
Bas <basarab.o@icloud.com>
2025-10-18 21:00:31 +0300

    add render

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,14 +2,53 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
+
 const lightbox = new SimpleLightbox('.gallery-link', {
+  captions: true,
   captionsData: 'alt',
   captionDelay: 250,
+  overlayOpacity: 1,
 });
 
 export function createGallery(images) {
-  return images.map(() => {
-    `
-        `;
-  });
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <li class="gallery-item">
+      <a class="gallery-link" href="${largeImageURL}">
+      <img class="gallery-image" src="${webformatURL}" alt="${tags}"/>
+      <ul class="gallery-desc">
+      <li class="gallery-info">
+      <span class="info-names">Likes</span>
+      <span class="info-item">${likes}</span>
+      </li>
+      <li class="gallery-info">
+      <span class="info-names">Views</span>
+      <span class="info-item">${views}</span>
+      </li>
+      <li class="gallery-info">
+      <span class="info-names">Comments</span>
+      <span class="info-item">${comments}</span>
+      </li>
+      <li class="gallery-info">
+      <span class="info-names">Downloads</span>
+      <span class="info-item">${downloads}</span>
+      </li>
+      </ul>
+      </a>
+      </li>
+        `
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
 }
